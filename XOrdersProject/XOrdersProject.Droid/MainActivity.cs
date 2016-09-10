ddd265ee834091ffe1bea15cf4adbde55fc41d68
David Zambrano <davazamb10@outlook.com>
2016-09-10 15:34:54 -0400

    Init

---
+++ XOrdersProject/XOrdersProject.Droid/MainActivity.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Android.Graphics.Drawables;
+
+namespace XOrdersProject.Droid
+{
+    [Activity(Label = "XOrdersProject", Icon = "@drawable/logo", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsApplicationActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            global::Xamarin.Forms.Forms.Init(this, bundle);
+            LoadApplication(new App());
+
+            //Eliminar icono aplicacion
+            ActionBar.SetIcon(
+                new ColorDrawable(Resources.GetColor(Android.Resource.Color.Transparent)));
+        }
+    }
+}
+
---
+++ XOrdersProject/XOrdersProject/Infrastructure/InstanceLocator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using XOrdersProject.ViewModels;
+
+namespace XOrdersProject.Infrastructure
+{
+    public class InstanceLocator
+    {
+        public InstanceLocator()
+        {
+            Main = new MainViewModel();
+        }
+        public MainViewModel Main { get; set; }
+    }
+}
